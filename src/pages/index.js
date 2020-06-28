4b5eeb7790f06ce96ace57d5c840066b2021c01e
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-06-27 21:19:57 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const HomePage = () => (
             ]}
           />
          <Profile
-          profile={[
+          images={[
           '/static/profile/perfil.png',
           ]}
           />
