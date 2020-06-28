a6eed1e667c45a0ff2ff14273ad58e66364dc426
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-06-27 21:06:23 -0300

    Update styles.js

--- src/components/Profile/styles.js
+++ src/components/Profile/styles.js
@@ -1,16 +1,6 @@
 mport styled from 'styled-components';
 
-export const SliderContainer = styled.section`
+export const ProfileContainer = styled.section`
   width: 100%;
-  height: 100%;
-`;
-
-export const Image = styled.img`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  left: 0;
-  object-fit: cover;
-  opacity: ${props => (props.selected ? 1 : 0)};
-  transition: opacity 300ms;
+  height: auto;
 `;
