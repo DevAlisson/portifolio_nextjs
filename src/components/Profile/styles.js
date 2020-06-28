6e08a6320d2a1571656839d145deb9be1cc9f758
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-06-27 21:13:39 -0300

    Update styles.js

--- src/components/Profile/styles.js
+++ src/components/Profile/styles.js
@@ -4,3 +4,13 @@ export const ProfileContainer = styled.section`
   width: 100%;
   height: auto;
 `;
+
+export const Image = styled.img`
+  position: absolute;
+  width: 100%;
+  height: auto%;
+  left: 0;
+  object-fit: cover;
+  opacity: ${props => (props.selected ? 1 : 0)};
+  transition: opacity 300ms;
+`;
