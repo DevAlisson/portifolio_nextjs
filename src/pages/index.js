a7033f818c1f1046aaf8cebd3550a17df4e81862
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-06-27 21:30:43 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const HomePage = () => (
             ]}
           />
           <div>
-            <img src="/static/profile/perfil.png alt="logo" width="512" height="auto">
+            <img src="/static/profile/perfil.png" alt="logo" width="512" height="auto">
            <title> AllissonGOD </title>
             <h1>
               Allisson<span>GOD</span>
