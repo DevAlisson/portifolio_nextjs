eee38e30e43b3f6d53de440a5feeedafa86dfca4
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-06-27 21:50:13 -0300

    Update index.js

--- src/styles/pages/index.js
+++ src/styles/pages/index.js
@@ -38,6 +38,15 @@ export const Content = styled.section`
       span {
         color: #0000ff;
       }
+ 
+     @keyframes pisca{
+        0%, 100%{
+        opacity: 1;
+    }
+        50%{
+        opacity: 0;
+      }
+   }
 
       @media (max-width: 515px) {
         display: flex;
