82582a9c15583752a744f1cfc7ca8920f4102de7
Alisson /Dev <37303237+DevAlisson@users.noreply.github.com>
2020-08-30 00:27:03 -0300

    Update gift.js

--- src/pages/gift.js
+++ src/pages/gift.js
@@ -5,7 +5,7 @@ import Layout from '~/layouts/Default';
 
 // import Slider from '~/components/Slider';
 
-import ContactPage from '~/pages/contact';
+// import ContactPage from '~/pages/contact';
 // import ProjectsSection from '~/pages/projects';
 
 import { HomeContainer, Content, Section, Player } from '~/styles/pages/gift';
@@ -49,7 +49,20 @@ const GiftPage = () => {
                o banner da tela de carregamento era so pra vc ver pois eu tirei
                mais o seu presente ta no meu site é secreto pois ngm sabe
                eu ia mostrar no nosso <b>aniversario de 4 meses de amizade</b>{' '}
-                mais o coração falou mais forte
+                mais o coração falou mais forte.
+    
+               Eu sei que to aqui com vc por tudo, a gente é amigo eu sei
+               so que eu quero te conhecer mais sem eu ficar pensando ela é ex do meu amigo
+               eu não vou desistir pois eu gosto de você esse presente so você sabe dele
+               eu quero que você esqueça ele você tem os meus braços pra chorar,
+               você tem meu carinho, eu sou romantico mesmo com amizade ( so de menina )
+               ele não merece você tem varias pessoas que vai atras de você ( mais não oque vc sente )
+               a maioria so quer curtição quer falar eu tenho namorada e vocês não mais ninguem sabe
+               que você ta em casa esperando ele pra ter um abraço gostoso ter aquele dia lindo
+               eu sei que somos amigo mais você tem os meus braços pra te guardar e não fique dando encima
+               dos cara la pois a Soraka ela so quer saber que o Batman ta sozinho vai pra cima e ela sabe
+               que vc gosta dele então a Ryuk e as meninas vão fazer vc ficar com ciumes... Te amo 
+               ( eu sei que não vai falar o mesmo :) )
               </p>
             </li>
           </ol>
@@ -58,8 +71,8 @@ const GiftPage = () => {
         {/* Projects SectionPage Component
         <ProjectsSection />* /}
 
-        {/* Contact SectionPage Component */}
-       <ContactPage />
+        {/* Contact SectionPage Component 
+       <ContactPage /> */}
       </HomeContainer>
     </Layout>
   );
